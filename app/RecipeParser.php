21c03da2cddfcc2478905d9f14022b14c49fd033
mrdth <mrdth@zeventien.org>
2023-06-07 21:34:22 +0100

    Add unit tests for RecipeParser class

--- app/RecipeParser.php
+++ app/RecipeParser.php
@@ -3,21 +3,22 @@
 namespace App;
 
 use Brick\StructuredData\Item;
+use Exception;
 use Illuminate\Support\Str;
 
 final class RecipeParser
 {
     public static function fromItems($items, $url): ?Recipe
     {
+        foreach ($items as $item) {
+            if (Str::contains(Str::lower(implode(',', $item->getTypes())), 'recipe')) {
+                return (new self(url: $url))->parse($item);
+            }
+        }
+
         // The whole thing might be a recipe
         if (count($items) == 1) {
             return (new self(url: $url))->parse($items);
-        }
-
-        foreach($items as $item) {
-            if(Str::contains(Str::lower(implode(',', $item->getTypes())), 'recipe')) {
-                return (new self(url: $url))->parse($item);
-            }
         }
 
         return null;
@@ -37,14 +38,23 @@ final class RecipeParser
 
     public function parse(Item $item): Recipe
     {
-        foreach($item->getProperties() as $name => $values) {
-            $fn = "parse_".Str::replace(['http://schema.org/', 'https://schema.org/'], '', Str::lower($name));
-            if(method_exists($this, $fn)) {
+        foreach ($item->getProperties() as $name => $values) {
+            $fn = "parse_" . Str::replace(['http://schema.org/', 'https://schema.org/'], '', Str::lower($name));
+            if (method_exists($this, $fn)) {
                 $this->$fn($values);
             }
         }
 
-        return new Recipe($this->title, $this->url, $this->author, $this->ingredients, $this->steps, $this->yield, $this->totalTime, $this->images);
+        return new Recipe(
+            $this->title,
+            $this->url,
+            $this->author,
+            $this->ingredients,
+            $this->steps,
+            $this->yield,
+            $this->totalTime,
+            $this->images
+        );
     }
 
     protected function parse_name($values): void
@@ -64,10 +74,9 @@ final class RecipeParser
 
     public function parse_image($values): void
     {
-        foreach($values as $item) {
+        foreach ($values as $item) {
             if ($item instanceof Item) {
-                foreach($item->getProperties() as $name => $values) {
-
+                foreach ($item->getProperties() as $name => $values) {
                     $name = Str::replace(['http://schema.org/', 'https://schema.org/'], '', Str::lower($name));
                     // $name may be one of [url, height, thumbnail, width]
                     if ($name == "url") {
@@ -79,7 +88,7 @@ final class RecipeParser
                 }
             } else {
                 if (is_array($item)) {
-                    throw new \Exception("Handle image items are array of strings");
+                    throw new Exception("Handle image items are array of strings");
                 } else {
                     if (Str::contains($item, ["http://", "https://"])) {
                         $this->images[] = $item;
@@ -92,18 +101,20 @@ final class RecipeParser
     public function parse_recipeingredient($values): void
     {
         if (is_array($values)) {
-            $this->ingredients = array_merge(collect($values)->transform(function ($item) {
-                return html_entity_decode($item);
-            })->toArray());
+            $this->ingredients = array_merge(
+                collect($values)->transform(function ($item) {
+                    return html_entity_decode($item);
+                })->toArray()
+            );
         }
     }
 
     public function parse_recipeinstructions($values): void
     {
-        foreach($values as $item) {
+        foreach ($values as $item) {
             if ($item instanceof Item) {
-                if(Str::contains(Str::lower(implode(',', $item->getTypes())), 'howtostep')) {
-                    foreach($item->getProperties() as $name => $values) {
+                if (Str::contains(Str::lower(implode(',', $item->getTypes())), 'howtostep')) {
+                    foreach ($item->getProperties() as $name => $values) {
                         $name = Str::replace(['http://schema.org/', 'https://schema.org/'], '', Str::lower($name));
                         if ($name == "text") {
                             $this->steps[] = html_entity_decode($values[0]);
---
+++ tests/Unit/RecipeParserTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Recipe;
+use App\RecipeParser;
+use Brick\StructuredData\HTMLReader;
+use Brick\StructuredData\Reader\JsonLdReader;
+
+function makeItems(): array
+{
+    $html = mb_convert_encoding(file_get_contents(__DIR__ . "/../data/recipe.html"), 'HTML-ENTITIES', "UTF-8");
+
+    return (new HTMLReader(new JsonLdReader()))->read($html, 'http://localhost');
+}
+
+it('can be instantiated', function () {
+    $parser = new RecipeParser();
+    expect($parser)->toBeInstanceOf(RecipeParser::class);
+});
+
+it('can be make a recipe from items', function () {
+    $items = makeItems();
+    $recipe = RecipeParser::fromItems($items, 'http://localhost');
+
+    expect($recipe)
+        ->toBeInstanceOf(Recipe::class)
+        ->toHaveProperties([
+            'title' => 'Air fryer chicken thighs',
+            'url' => 'http://localhost',
+            'ingredients' => [
+                "1 tsp paprika",
+                "½ tsp  mixed herbs",
+                "½ tsp  garlic granules (optional)",
+                "4 chicken thighs, bone in",
+                "1 tsp olive oil",
+            ],
+            'steps' => [
+                "Combine the paprika in a bowl with the herbs and garlic granules, if using, together with ½ tsp salt and ½ tsp ground black pepper. Scatter onto a plate. Rub the chicken thighs with the oil, then coat in the spice mix.",
+                "Put in the basket of your air fryer and cook, skin-side down, for 10 mins at 180C. Turn over and cook for a further 10-15 mins until cooked through and the skin is crispy. To check they are cooked, pierce the thickest part of the thigh with a knife to see if the juices run clear. Remove from the air fryer immediately to stop the skin from softening.",
+            ],
+            'yield' => 4,
+            'totalTime' => "PT27M",
+            'images' => [
+                "https://images.immediate.co.uk/production/volatile/sites/30/2022/04/Air-Fryer-Chicken-Thighs-d4575b2.jpg?resize=768,574",
+            ],
+        ]);
+});
