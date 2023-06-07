d95a3d34593a8e5b9ecbe56f946a0082539e08a7
mrdth <mrdth@zeventien.org>
2023-06-07 18:22:28 +0100

    Remove phpstan temp folder from vcs

--- .phpstan/cache/PHPStan/6d/d9/6dd9d8c1bb3fd9ff23d5925a773fe8067c3a089c.php
+++
@@ -1,6 +0,0 @@
-<?php declare(strict_types = 1);
-
-return \PHPStan\Cache\CacheItem::__set_state(array(
-   'variableKey' => '1686158093-v4',
-   'data' => false,
-));
--- .phpstan/cache/PHPStan/db/71/db71256cd1ce0c591e782781fc2bb8aea68e7050.php
+++
@@ -1,6 +0,0 @@
-<?php declare(strict_types = 1);
-
-return \PHPStan\Cache\CacheItem::__set_state(array(
-   'variableKey' => '1686158058-v4',
-   'data' => false,
-));
