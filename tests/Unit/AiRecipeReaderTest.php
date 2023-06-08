45695aa6579ab4abbdc2ede069df14963a02382d
mrdth <mrdth@zeventien.org>
2023-06-08 21:00:19 +0100

    Refactor RecipeParserTest, AiRecipeReaderTest, and FetchRecipeTest to use Recipe model, and correctly assert against it's attributes

--- tests/Unit/AiRecipeReaderTest.php
+++ tests/Unit/AiRecipeReaderTest.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Actions\AIRecipeReader;
-use App\Recipe;
+use App\Models\Recipe;
 use Illuminate\Support\Facades\File;
 use OpenAI\Responses\Chat\CreateResponse;
 use OpenAI\Responses\Chat\CreateResponseMessage;
@@ -28,9 +28,10 @@ test('example', function () {
 
     expect($recipe)
         ->toBeInstanceOf(Recipe::class)
-        ->toHaveProperties([
+        ->and($recipe->attributesToArray())->toBe([
             'title' => 'Air fryer chicken thighs',
             'url' => 'https://www.bbcgoodfood.com/recipes/air-fryer-chicken-thighs#Recipe',
+            'author' => '',
             'ingredients' => [
                 "1 tsp paprika",
                 "½ tsp  mixed herbs",
@@ -42,10 +43,11 @@ test('example', function () {
                 "Combine the paprika in a bowl with the herbs and garlic granules, if using, together with ½ tsp salt and ½ tsp ground black pepper. Scatter onto a plate. Rub the chicken thighs with the oil, then coat in the spice mix.",
                 "Put in the basket of your air fryer and cook, skin-side down, for 10 mins at 180C. Turn over and cook for a further 10-15 mins until cooked through and the skin is crispy. To check they are cooked, pierce the thickest part of the thigh with a knife to see if the juices run clear. Remove from the air fryer immediately to stop the skin from softening.",
             ],
-            'yield' => 4,
+            'yield' => '4',
             'totalTime' => "PT27M",
             'images' => [
                 "https://images.immediate.co.uk/production/volatile/sites/30/2022/04/Air-Fryer-Chicken-Thighs-d4575b2.jpg?resize=768,574",
             ],
+            'ai_generated' => true,
         ]);
 });
--- tests/Unit/RecipeParserTest.php
+++ tests/Unit/RecipeParserTest.php
@@ -9,7 +9,10 @@ function makeItems(): array
 {
     $html = mb_convert_encoding(file_get_contents(__DIR__ . "/../data/recipe.html"), 'HTML-ENTITIES', "UTF-8");
 
-    return (new HTMLReader(new JsonLdReader()))->read($html, 'https://www.bbcgoodfood.com/recipes/air-fryer-chicken-thighs#Recipe');
+    return (new HTMLReader(new JsonLdReader()))->read(
+        $html,
+        'https://www.bbcgoodfood.com/recipes/air-fryer-chicken-thighs#Recipe'
+    );
 }
 
 it('can be instantiated', function () {
@@ -22,10 +25,11 @@ it('can be make a recipe from items', function () {
     $recipe = RecipeParser::fromItems($items, 'https://www.bbcgoodfood.com/recipes/air-fryer-chicken-thighs#Recipe');
 
     expect($recipe)
-        ->toBeInstanceOf(Recipe::class)
-        ->toHaveProperties([
+        ->toBeInstanceOf(\App\Models\Recipe::class)
+        ->and($recipe->attributesToArray())->toBe([
             'title' => 'Air fryer chicken thighs',
             'url' => 'https://www.bbcgoodfood.com/recipes/air-fryer-chicken-thighs#Recipe',
+            'author' => '',
             'ingredients' => [
                 "1 tsp paprika",
                 "½ tsp  mixed herbs",
@@ -37,7 +41,7 @@ it('can be make a recipe from items', function () {
                 "Combine the paprika in a bowl with the herbs and garlic granules, if using, together with ½ tsp salt and ½ tsp ground black pepper. Scatter onto a plate. Rub the chicken thighs with the oil, then coat in the spice mix.",
                 "Put in the basket of your air fryer and cook, skin-side down, for 10 mins at 180C. Turn over and cook for a further 10-15 mins until cooked through and the skin is crispy. To check they are cooked, pierce the thickest part of the thigh with a knife to see if the juices run clear. Remove from the air fryer immediately to stop the skin from softening.",
             ],
-            'yield' => 4,
+            'yield' => '4',
             'totalTime' => "PT27M",
             'images' => [
                 "https://images.immediate.co.uk/production/volatile/sites/30/2022/04/Air-Fryer-Chicken-Thighs-d4575b2.jpg?resize=768,574",
