1594c66430fec4a3a30f5b3157c6caa077607822
mrdth <mrdth@zeventien.org>
2023-06-09 14:04:10 +0100

    Add RecipeFactory and RecipeSeeder with Recipe model to seed the database.

--- database/factories/RecipeFactory.php
+++ database/factories/RecipeFactory.php
@@ -2,10 +2,13 @@
 
 namespace Database\Factories;
 
+use App\Models\Recipe;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\File;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Recipe>
+ * @extends Factory<Recipe>
  */
 class RecipeFactory extends Factory
 {
@@ -16,8 +19,17 @@ class RecipeFactory extends Factory
      */
     public function definition(): array
     {
+        $data = File::json(__DIR__ . '/recipe_data.json');
+        
         return [
-            //
+            'title' => Arr::random($data['titles'], 1),
+            'url' => fake()->url,
+            'author' => fake()->name,
+            'ingredients' => Arr::random($data['ingredients'], fake()->numberBetween(7, 20)),
+            'steps' => Arr::random($data['steps'], fake()->numberBetween(2, 4)),
+            'yield' => fake()->numberBetween(2, 6),
+            'totalTime' => Arr::random($data['totalTime'], 1),
+            'images' => Arr::random($data['images'], 1),
         ];
     }
 }
